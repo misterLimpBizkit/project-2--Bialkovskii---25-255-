928f95d9cd3bd7e3542daa87024ecc8475d2dff2
MisterSigmaMac <egorzapekanka@gmail.com>
2025-11-13 16:57:08 +0300

    Настроено окружение, добавлены библиотеки и настроен ruff

---
+++ src/primitive_db/engine.py
@@ -0,0 +1,17 @@
+import prompt
+
+
+def welcome():
+    print("Добро пожаловать в DB-проект!")
+    print("Доступные команды: 'help', 'exit'")
+    while True:
+        answer = prompt.string('Введите команду: ').strip().lower()
+        if answer == 'exit':
+            break
+        if answer == 'help':
+            print("<command> exit - выйти из программы")
+            print("<command> help - справочная информация")
+        else:
+            print('Неизвестная команда')
+    
+        
--- src/primitive_db/main.py
+++ src/primitive_db/main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+from engine import welcome
+
+
 def main():
     print('DB project is running!')
+    welcome()
 
 if __name__ == '__main__':
     main()
